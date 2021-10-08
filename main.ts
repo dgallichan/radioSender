4cfafcc3bbee2ec641020189c87c2e6179b9ddf7
Daniel Gallichan <dgallichan@yahoo.com>
2021-10-08 17:09:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ radio.setGroup(99)
 basic.forever(function () {
     radio.sendValue("acc", input.acceleration(Dimension.Strength))
     led.toggle(2, 2)
-    basic.pause(500)
+    basic.pause(100)
 })
