947df1bda3f86c75f4eae700837ef5ce65e01bea
Daniel Gallichan <dgallichan@yahoo.com>
2021-10-19 13:07:58 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 radio.setGroup(99)
+basic.showLeds(`
+    # # # . .
+    . # . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
     radio.sendValue("acc", input.acceleration(Dimension.Strength))
-    led.toggle(2, 2)
-    basic.pause(100)
+    led.toggle(4, 4)
+    basic.pause(5)
 })
